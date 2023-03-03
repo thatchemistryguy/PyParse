25f744b99e92bb5d90f01fe89b64468fa8c4ba80
Joe Mason <joseph.d.mason@gsk.com>
2023-03-03 13:17:10 +0000

    trim docs
    Update docs to ensure correct structure

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('C:/Users/jdm73868/OneDrive - GSK/Documents/Cheminformatic Tools/PythonReader/Publication/PyParse'))
+sys.path.insert(0, os.path.abspath('..'))
 import PyParse
 
 # -- Project information -----------------------------------------------------
