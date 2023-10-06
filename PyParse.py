8661ab9c3babd3aeb989dc0501001bf2667aad08
Joe Mason <128401233+thatchemistryguy@users.noreply.github.com>
2023-10-06 13:05:06 +0100

    Update UI-tweaks
    Include detector option from ELSD branch

--- PyParse.py
+++ PyParse.py
@@ -2351,6 +2351,9 @@ def main():
     
     parser.add_argument("-n", "--name", action="store", type=str, dest = "analysis_name", 
                         help = "Choose a name for the analysis.\n")
+                        
+    parser.add_argument("-d", "--detector", action="store", type=str, dest = "detector",
+                        help = "Choose which detector to use, UV or ELSD")
 
 
     #Set options to global and parse arguments        
